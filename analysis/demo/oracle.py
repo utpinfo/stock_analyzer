62c014076235ddcc3008fd8a640cade6424f3852
yangfengkai <yangfk0128@gmail.com>
2025-10-31 01:25:02 +0800

    support oracle connect

---
+++ analysis/demo/oracle.py
@@ -0,0 +1,18 @@
+"""
+# 前置作業
+1. 安裝 pip install cx_Oracle
+2. 部屬 Oracle Client
+"""
+import cx_Oracle
+from sqlalchemy import create_engine, text
+
+# ✅ 指定 Oracle Client 目錄
+cx_Oracle.init_oracle_client(lib_dir="/Users/yangfengkai/Documents/instantclient")
+
+# ✅ 建立連線引擎
+engine = create_engine('oracle+cx_oracle://prg:prg7695@116.236.30.11:1521/?service_name=mis.gs.com.cn')
+
+# ✅ 正確用法：使用 text() 包裝 SQL
+with engine.connect() as conn:
+    data = conn.execute(text("SELECT * FROM idm_user WHERE ROWNUM < 10"))
+    print(data.fetchall())
